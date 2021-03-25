640d541f69ed5ad21ca0776db386f8fc5f2933c3
Aanyakk <73647559+Aanyakk@users.noreply.github.com>
2021-03-25 17:53:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -53,8 +53,8 @@ function gotResult(error, results) {
         console.log(results);
         document.getElementById("result_emotion_name").innerHTML = results[0].label;
         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-        prediction_1 = results[0].label
-        prediction_2 = results[1].label
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
         speak();
         if(results[0].label == "happy")
         {
